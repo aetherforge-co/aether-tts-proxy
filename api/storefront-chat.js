e616318a1db7a611622a6f1f6b7ac4fb33a2fb86
aetherforge-co <aetherforge.eng@gmail.com>
2026-07-30 08:28:46 -0500

    Update storefront-chat.js

--- api/storefront-chat.js
+++ api/storefront-chat.js
@@ -130,7 +130,7 @@ module.exports = async function handler(req, res) {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         contents,
-        generationConfig: { maxOutputTokens: 150 },
+        generationConfig: { maxOutputTokens: 600, thinkingConfig: { thinkingBudget: 0 } },
       }),
     });
 
